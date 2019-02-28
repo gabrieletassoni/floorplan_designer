2e7bdb3b61e66442802b976ee0a378ad9d3d1c09
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-02-28 12:55:46 +0100

    Add cdn for packery

--- lib/thecore_floorplan_designer.rb
+++ lib/thecore_floorplan_designer.rb
@@ -1,4 +1,4 @@
-require "thecore_floorplan_designer/engine"
+require 'thecore_floorplan_designer/engine'
 
 require 'thecore'
 module ThecoreFloorplanDesigner
@@ -12,51 +12,46 @@ module RailsAdmin
     module Actions
       class ThecoreFloorplanDesigner < Base
         RailsAdmin::Config::Actions.register(self)
-      register_instance_option :object_level do
+        register_instance_option :object_level do
           false
-      end
-		# This ensures the action only shows up for Users
-		register_instance_option :visible? do
-		    # visible only if authorized and if the object has a defined method
-		    authorized? #&& bindings[:abstract_model].model == ThecoreFloorplanDesigner && bindings[:abstract_model].model.column_names.include?('barcode')
-		end
-		# We want the action on members, not the Users collection
-		register_instance_option :member do
-		    false
-		end
-		
-		register_instance_option :collection do
-		    false
-		end
-		register_instance_option :root? do
-		    true
-		end
-		
-		register_instance_option :breadcrumb_parent do
-		    [nil]
-		end
-		
-		register_instance_option :link_icon do
-		    'icon-barcode'
-		end
-		# You may or may not want pjax for your action
-		register_instance_option :pjax? do
-		    true
-		end
-		# Adding the controller which is needed to compute calls from the ui
-		register_instance_option :controller do
-		    Proc.new do # This is needed becaus we sant that this code is re-evaluated each time is called
-		        # This could be useful to distinguish between ajax calls and restful calls
-		        if request.xhr?
-		        end
-		    end
-		end
+        end
+        # This ensures the action only shows up for Users
+        register_instance_option :visible? do
+          # visible only if authorized and if the object has a defined method
+          authorized? # && bindings[:abstract_model].model == ThecoreFloorplanDesigner && bindings[:abstract_model].model.column_names.include?('barcode')
+        end
+        # We want the action on members, not the Users collection
+        register_instance_option :member do
+          false
+        end
 
+        register_instance_option :collection do
+          false
+        end
+        register_instance_option :root? do
+          true
+        end
 
-        
-        
+        register_instance_option :breadcrumb_parent do
+          [nil]
+        end
+
+        register_instance_option :link_icon do
+          'icon-barcode'
+        end
+        # You may or may not want pjax for your action
+        register_instance_option :pjax? do
+          true
+        end
+        # Adding the controller which is needed to compute calls from the ui
+        register_instance_option :controller do
+          proc do # This is needed becaus we sant that this code is re-evaluated each time is called
+            # This could be useful to distinguish between ajax calls and restful calls
+            if request.xhr?
+            end
+          end
+        end
       end
     end
   end
 end
-
