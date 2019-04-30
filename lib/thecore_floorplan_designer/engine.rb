82d88b37873fdf4dd13af79c79599651fecf7f9c
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:33:10 +0200

    migration initializer for engine cleanup

--- lib/thecore_floorplan_designer/engine.rb
+++ lib/thecore_floorplan_designer/engine.rb
@@ -6,7 +6,7 @@ module ThecoreFloorplanDesigner
     end
 
     initializer 'thecore_floorplan_designer.add_to_migrations' do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
